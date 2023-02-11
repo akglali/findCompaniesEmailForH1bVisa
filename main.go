6c901450c2e29e6962295c1dffa05c7c9cbd8a4e
akglali <akgol97_@hotmail.com>
2023-02-10 20:51:13 -0500

    last fixed are made before sending emails

--- main.go
+++ main.go
@@ -1,20 +1,28 @@
 package main
 
-import (
-	"findCompaniesEmailForH1bVisa/companiesEmail"
-	"findCompaniesEmailForH1bVisa/companiesName"
-	"findCompaniesEmailForH1bVisa/companiesWebsites"
-	"findCompaniesEmailForH1bVisa/sendEmailToFoundCompanies"
-)
+import "findCompaniesEmailForH1bVisa/sendEmailToFoundCompanies"
 
 func main() {
+	// **** h1bgrader.com version
 	//first getting the companiesName name from h1b grader website for software developer position
-	companiesName.GetCompaniesName()
+	//companiesName.GetCompaniesName()
 	//then find the companiesName website according to their name
-	companiesWebsites.FindWebsites()
+	//companiesWebsites.FindWebsites()
 	//time to find companies email if it is existed and save them to use it.
-	companiesEmail.FindEmail()
+	//companiesEmail.FindEmail()
 	//last step send the all found emails.
-	sendEmailToFoundCompanies.SendEmail()
+	//sendEmailToFoundCompanies.SendEmail()
 
+	// **** Google Map Search Version
+	//get all the companies information
+	//googleMapsSearch.getCompaniesNameByGoogleSearch()
+
+	//find all the companies website by their name
+	//googleMapsSearch.FindWebsites()
+
+	//find companies email from their websites
+	//googleMapsSearch.FindEmailGoogleMap()
+
+	//send emails
+	sendEmailToFoundCompanies.SendEmails()
 }
--- sendEmailToFoundCompanies/sentEmail.go
+++ sendEmailToFoundCompanies/sentEmail.go
@@ -15,7 +15,7 @@ import (
 	"time"
 )
 
-func SendEmail() {
+func SendEmails() {
 	// to be able to read .env file we should load first
 	err := godotenv.Load()
 	if err != nil {
@@ -25,7 +25,7 @@ func SendEmail() {
 
 	emptyEmailFile := excelize.NewFile()
 
-	f, err := excelize.OpenFile("employerH1B.xlsx")
+	f, err := excelize.OpenFile("employerForMapSearch.xlsx")
 	if err != nil {
 		log.Fatal(err)
 	}
@@ -36,9 +36,9 @@ func SendEmail() {
 	startTimer := 0
 	for index, row := range rows {
 		timer := time.After(4 * time.Minute)
-		companyName := row[1]
-		companyEmails := row[4]
-		companyWebsite := row[3]
+		companyName := row[0]
+		companyEmails := row[2]
+		companyWebsite := row[1]
 		if companyEmails != "[]" {
 			startTimer += 1
 			fmt.Println("Start Timer", startTimer)
@@ -46,7 +46,6 @@ func SendEmail() {
 				fmt.Println("stopped")
 				// Wait for timer to finish because otherwise smtp error will raise for requesting too much email
 				<-timer
-
 			}
 			// Remove square brackets from string
 			array := companyEmails[1 : len(companyEmails)-1]
@@ -61,7 +60,7 @@ func SendEmail() {
 						fmt.Println("don't send", email)
 					} else {
 						fmt.Println(email, index)
-						emailTemplate(companyName, email)
+						emailTemplateForGoogleMapCompanies(companyName, email)
 
 					}
 				}
@@ -86,7 +85,7 @@ func SendEmail() {
 
 //by default, it is .env, so we don't have to write
 
-func emailTemplate(companyName, email string) {
+func emailTemplateForGoogleMapCompanies(companyName, email string) {
 	fmt.Println(email)
 	err := godotenv.Load()
 	if err != nil {
@@ -102,7 +101,7 @@ func emailTemplate(companyName, email string) {
 		"I hope this email finds you well. I am writing to express my interest in a software engineering role at " + companyName +
 		". As a recently graduated computer engineer with a 3.35 GPA, I am eager to put my skills and knowledge to use in a challenging and dynamic work environment." +
 		"\n\nOne of my main objectives is to pursue a career in the United States, and I believe that " + companyName + " would provide me with the opportunities I am seeking." +
-		" I am highly motivated, ambitious and eager to take on new challenges, and I believe that I would be an asset to your team.\n\n" +
+		" I am highly motivated, ambitious and eager to take on new challenges, and I believe that I would be an asset to your team. Kindly consider supporting my aspirations and please do not let my dreams go unfulfilled.\n\n" +
 		"I have a strong foundation in various programming languages such as Go, Java, Python, TypeScript, JavaScript, SQL, and Dart." +
 		" I am proficient in front-end frameworks like React, Angular, NextJS, and Flutter, and I have experience in creating robust and scalable" +
 		" web applications using backend frameworks like ExpressJS, NestJS, and Gin.\n\nI am confident that my technical skills, combined with my passion" +
@@ -110,10 +109,17 @@ func emailTemplate(companyName, email string) {
 		"to have the opportunity to bring my skills and experience to your team and to be a part of a company that is making a difference in the industry.\n\n" +
 		"I would appreciate the opportunity to discuss my qualifications and how I can contribute to the success of your company. I am available for an interview at your convenience. " +
 		"I have attached my resume and portfolio for your review.\n\n" +
-		"NOTE: I was searching for a company that can offer me H1B visa sponsorship," + "We then searched for email addresses on these websites by writing a function that" +
-		" searched for them in the text. To send the emails, we created a template email and wrote another function to send the emails. " +
-		"Although writing the code wasn't particularly difficult, I believe it demonstrates my dedication and determination to make my dream of" +
-		" starting my career in the US a reality. The code is available on GitHub! \n\n" +
+		"NOTE: My friend and I were on a mission to find a company that could offer us H1B visa sponsorship. To accomplish this," +
+		" we utilized Google Maps API to gather information on IT companies in the 50 states and 10 most populous cities. This search " +
+		"resulted in over 8,000 company names, which we then researched further through Bing search and retrieved their websites. To make contact," +
+		" we wrote a function to search for email addresses on these websites and crafted a template email. Sending the emails handled by another function. " +
+		"Although the coding process was not particularly challenging, it showcased my determination and commitment to pursuing my career in the US." +
+		" The code is available on GitHub for review \n\n" +
+		"Note2: I arrived in the United States through the work and travel program with a J1 visa." +
+		" Presently, my visa status is B2 as a visitor. I am in the process of enrolling in a school program" +
+		" to change my visa status to F1. I would be so happy If you can file for me H1B on March so I can legally start working for you on October and meanwhile I can " +
+		"learn everything that the " + companyName + " required and I can help as a volunteer :). Also, OPT or CPT are financially not feasible for me at this time." +
+		" If your company is interested in sponsoring me for OPT or CPT, I would be grateful for the opportunity and express my sincere enthusiasm for joining your team.\n\n" +
 		"https://github.com/akglali/findCompaniesEmailForH1bVisa \n\n" +
 		"\n\nThank you for considering my application. I look forward to hearing from you soon." +
 		"\n\nBest regards," +
